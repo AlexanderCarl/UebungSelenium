e73c15b6d264c208afbdd95fa2dcf5e1c0e658ae
Alexander Carl <alexander.carl@adesso.de>
2024-09-04 11:45:14 +0200

    Out-of-Training-Scope Added parallel thread execution

--- src/test/java/selenium/tests/GithubTests.java
+++ src/test/java/selenium/tests/GithubTests.java
@@ -20,35 +20,43 @@ public class GithubTests extends BaseTest {
     private static final Logger logger = LoggerFactory.getLogger(Utils.class);
     
     @Test
-    public void displayGitHubSimple() {
+    public void displayHomepage() {
+        
         // Step 1
         HomePage homePage = this.defaultLogin();
-        
+    
         // Step 2
-        logger.info("There are " + homePage.getBoardPage().getNumberOfCommits());
+        logger.info("There are " + homePage.getBoardPage().getNumberOfCommits() + " number of commits");
         logger.info("The current release version is " + homePage.getBoardPage().getCurrentReleaseVersion());
+    }
+    
+    @Test
+    public void filterIssues() {
         
-        // Step 3
+        // Step 1
+        HomePage homePage = this.defaultLogin();
+    
+        // Step 2
         IssuesPage issuesPage = homePage.getHeaderPage().goToIssues();
         issuesPage.setLabelFilter(IssuesLabelFilter.JAVA);
         issuesPage.setSortByFilter(IssuesSortByFilter.MOST_COMMENTED);
-        
-        // Step 4
+    
+        // Step 3
         List<Integer> sortedIssuesComments = issuesPage.getAllShownIssuesComments();
         if (!sortedIssuesComments.isEmpty()) {
             boolean isDescending = IntStream.range(0, sortedIssuesComments.size() - 1)
                     .allMatch(i -> sortedIssuesComments.get(i) >= sortedIssuesComments.get(i + 1));
             Assert.assertTrue(isDescending, "The Issues should be ordered from the most commented to the least commented ones.");
         }
-        
-        // Step 5
+    
+        // Step 4
         Issue issue = issuesPage.getIssue(0);
         String tags = String.join(" ", issue.tags());
         String issueInfo = "Most commentend infos:" +
                 "\nTitle: " + issue.header() +
                 "\ntags: " + tags +
                 "\nopened: " + issue.shortInfo();
-        
+    
         logger.info(issueInfo);
     }
 }
--- src/test/java/selenium/tests/common/BaseTest.java
+++ src/test/java/selenium/tests/common/BaseTest.java
@@ -20,68 +20,85 @@ import java.util.Properties;
 public class BaseTest {
     
     private static final Browser browser = Utils.getConfiguredBrowser();
-    private BrowserWebDriverContainer container;
-    protected WebDriver driver;
+    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    private static ThreadLocal<BrowserWebDriverContainer> tlContainer = new ThreadLocal<>();
     protected Properties prop;
 
     protected String baseUrl;
     
     @BeforeTest
-    public void baseInit() {
+    public void prepareInit() {
         prop = Utils.getProp();
         baseUrl = prop.getProperty("baseUrl");
-        
-        setupDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        driver.manage().window().maximize();
     }
     
-    @AfterTest
-    public void shutDown() {
-        driver.quit();
-        if (browser != Browser.LOCAL) {
-            container.stop();
-        }
+    @BeforeTest
+    public void baseInit() {
+        tlDriver.set(getDriver(Utils.getConfiguredBrowser()));
+        tlDriver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        tlDriver.get().manage().window().maximize();
     }
     
-    // Necessary if multiple tests are run in a row as we keep the container & driver alive for multiple tests
+    // Necessary as multiple tests are run in a row while the container & driver is alive for all tests of a test-path e.g. Test1 & Test2
     @AfterMethod
     public void clearBrowserData() {
+        WebDriver driver = tlDriver.get();
+        
         // Clear cookies
         driver.manage().deleteAllCookies();
-    
+        
         // Clear local storage and session storage
         ((JavascriptExecutor) driver).executeScript("window.localStorage.clear();");
         ((JavascriptExecutor) driver).executeScript("window.sessionStorage.clear();");
-    
+        
         // Navigate to a blank page to reset the browser context
         driver.get("about:blank");
     }
     
+    @AfterTest
+    public void shutDown() {
+        WebDriver driver = tlDriver.get();
+        driver.quit();
+        tlDriver.remove(); // clean up to avoid memory leaks
+        
+        if (browser != Browser.LOCAL) {
+            tlContainer.get().stop();
+            tlContainer.remove();
+        }
+    }
+    
     public HomePage defaultLogin() {
+        WebDriver driver = tlDriver.get();
         driver.get(baseUrl);
         
         return new HomePage(driver);
     }
     
-    private void setupDriver() {
-        switch (Utils.getConfiguredBrowser()) {
+    private WebDriver getDriver(Browser browser) {
+         switch (browser) {
             case CHROME:
                 ChromeOptions chromeOptions = new ChromeOptions();
-                container = new BrowserWebDriverContainer<>().withCapabilities(chromeOptions);
-                container.start();
-                driver = new RemoteWebDriver(container.getSeleniumAddress(), chromeOptions);
+                BrowserWebDriverContainer<?> chromeContainer = new BrowserWebDriverContainer<>().withCapabilities(chromeOptions);
+                chromeContainer.start();
+                tlContainer.set(chromeContainer);
+                return new RemoteWebDriver(chromeContainer.getSeleniumAddress(), chromeOptions);
+                
             case FIREFOX:
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
-                container = new BrowserWebDriverContainer<>().withCapabilities(firefoxOptions);
-                container.start();
-                driver = new RemoteWebDriver(container.getSeleniumAddress(), firefoxOptions);
+                BrowserWebDriverContainer<?> firefoxContainer = new BrowserWebDriverContainer<>().withCapabilities(firefoxOptions);
+                firefoxContainer.start();
+                return new RemoteWebDriver(firefoxContainer.getSeleniumAddress(), firefoxOptions);
+                
+            // Local browser is for dev use only and does not support parallel execution
             case LOCAL:
                 ChromeOptions localChromeOptions = new ChromeOptions();
                 Properties prop = Utils.getProp();
                 System.setProperty("webdriver.chrome.driver", prop.getProperty("locationChromeDriver"));
                 localChromeOptions.setBinary(prop.getProperty("locationChromeBrowser"));
-                driver = new ChromeDriver(localChromeOptions);
+                return new ChromeDriver(localChromeOptions);
+                
+            default:
+                return getDriver(Browser.CHROME);
         }
     }
 }
